5b6c689639164347985bcb1638d6c4ab275bd3ee
ricomuh <rico.smartlolo@gmail.com>
2023-04-22 18:42:22 +0700

    set the documentation

--- engine/Helper/Collection.php
+++ engine/Helper/Collection.php
@@ -6,6 +6,11 @@ class Collection implements \ArrayAccess, \IteratorAggregate, \Countable
 {
     protected $items = [];
 
+    /**
+     * Collection constructor.
+     * 
+     * @param array $items
+     */
     public function __construct(array $items = [])
     {
         $itemsToSet = [];
@@ -21,31 +26,56 @@ class Collection implements \ArrayAccess, \IteratorAggregate, \Countable
         $this->items = $itemsToSet;
     }
 
+    /**
+     * @param array $items
+     * @return static
+     */
     public function all()
     {
         return $this->items;
     }
 
+    /**
+     * @param $key
+     * @param null $default
+     * @return mixed|null
+     */
     public function count(): int
     {
         return count($this->items);
     }
 
+    /**
+     * @return \ArrayIterator|\Traversable
+     */
     public function getIterator(): \ArrayIterator
     {
         return new \ArrayIterator($this->items);
     }
 
+    /**
+     * @param $offset
+     * @return bool
+     */
     public function offsetExists($offset): bool
     {
         return isset($this->items[$offset]);
     }
 
+    /**
+     * @param $offset
+     * @return mixed
+     */
     public function offsetGet($offset)
     {
         return $this->items[$offset];
     }
 
+    /**
+     * @param $offset
+     * @param $value
+     * @return void
+     */
     public function offsetSet($offset, $value): void
     {
         if (is_null($offset)) {
@@ -55,12 +85,22 @@ class Collection implements \ArrayAccess, \IteratorAggregate, \Countable
         }
     }
 
+    /**
+     * @param $offset
+     * @return void
+     */
     public function offsetUnset($offset): void
     {
         unset($this->items[$offset]);
     }
 
-    public function forEach(callable $callback)
+    /**
+     * This method is used to foreach collection
+     * 
+     * @param $callback callable
+     * @return void
+     */
+    public function forEach(callable $callback): void
     {
         foreach ($this->items as $key => $value) {
             if ($callback($value, $key) === false) {
@@ -69,7 +109,13 @@ class Collection implements \ArrayAccess, \IteratorAggregate, \Countable
         }
     }
 
-    public function map(callable $callback)
+    /**
+     * This method is used to map collection
+     * 
+     * @param $callback callable
+     * @return Collection
+     */
+    public function map(callable $callback): Collection
     {
         $result = [];
 
@@ -80,7 +126,13 @@ class Collection implements \ArrayAccess, \IteratorAggregate, \Countable
         return new static($result);
     }
 
-    public function filter(callable $callback)
+    /**
+     * This method is used to filter collection
+     * 
+     * @param $callback callable
+     * @return Collection
+     */
+    public function filter(callable $callback): Collection
     {
         $result = [];
 
@@ -93,7 +145,12 @@ class Collection implements \ArrayAccess, \IteratorAggregate, \Countable
         return new static($result);
     }
 
-    public function toArray()
+    /**
+     * This method is used to convert collection to array
+     * 
+     * @return array
+     */
+    public function toArray(): array
     {
         $result = [];
 
@@ -108,17 +165,33 @@ class Collection implements \ArrayAccess, \IteratorAggregate, \Countable
         return $result;
     }
 
-    public function toJson()
+    /**
+     * This method is used to convert collection to json string
+     * 
+     * @return string
+     */
+    public function toJson(): string
     {
-        return json_encode($this->items);
+        return json_encode($this->toArray());
     }
 
-    public function __toString()
+    /**
+     * This method is used to convert collection to string
+     * 
+     * @return string
+     */
+    public function __toString(): string
     {
         return $this->toJson();
     }
 
-    public function add($item)
+    /**
+     * This method is used to add item to collection
+     * 
+     * @param $item
+     * @return void
+     */
+    public function add($item): void
     {
         if (is_array($item)) {
             $item = new static($item);
@@ -127,5 +200,16 @@ class Collection implements \ArrayAccess, \IteratorAggregate, \Countable
         }
 
         $this->items[] = $item;
+    }
+
+    /**
+     * This method is used to add custom method to collection class
+     * 
+     * @param $method string
+     * @param $callback callable
+     */
+    public function addMethod($method, $callback)
+    {
+        $this->$method = $callback;
     }
 }
