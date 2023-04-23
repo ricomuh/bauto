4d07be08ef9442357ad82a6814a9774decc3e9b8
ricomuh <rico.smartlolo@gmail.com>
2023-04-23 22:34:10 +0700

    env function

--- engine/Helper/helpers.php
+++ engine/Helper/helpers.php
@@ -165,3 +165,42 @@ if (!function_exists('asset')) {
         return url($path);
     }
 }
+
+if (!function_exists('env')) {
+    /**
+     * Get the value of an environment variable.
+     * 
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+
+        if ($value === false) {
+            return $default;
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+
+        if (strlen($value) > 1 && str_starts_with($value, '"') && str_ends_with($value, '"')) {
+            return substr($value, 1, -1);
+        }
+
+        return $value;
+    }
+}
